4def2e382be6d1bc6d65e4f72c370cb8e4ca9d23
Your Name <aroseca15@gmail.com>
2021-10-04 16:24:45 -0700

    started work on the NoteDisplayCard

--- react_work/src/components/NoteDisplayCard.js
+++ react_work/src/components/NoteDisplayCard.js
@@ -3,10 +3,21 @@
 // import React, { useState } from "react";
 
 function NoteDisplayCard() {
-    return(
+    return (
         <form>
-            <div className= "card">
-        
+            <div className="card recentDisplayCard">
+                <form>
+                    <div className="">
+                        <h6>Patient1 Name</h6>
+                        <button>See All</button>
+                    </div>
+                    <div>
+                        <p>Date and Time</p>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                        <button>View</button>
+                    </div>
+                </form>
             </div>
         </form>
     )
