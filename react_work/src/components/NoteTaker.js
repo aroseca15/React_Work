23e9dc35c90462ea612c6f396100f68e63ff520a
Your Name <aroseca15@gmail.com>
2021-10-04 15:52:17 -0700

    NoteTake Component finished.

--- react_work/src/components/NoteTaker.js
+++ react_work/src/components/NoteTaker.js
@@ -5,15 +5,15 @@
 function NoteTaker() {
     return (
 
-        <div className="card m-5 p-5">
+        <div className="card noteTakerCard m-5 p-5">
             <h3 className="mb-5">New Note</h3>
             <form>
                 <div className="notetakerR1 mb-5">
-                    <h4>Session Information:</h4>
+                    <h4>Session:</h4>
                     <div className="form-group col-md-4 me-1">
                         <input type="email" className="form-control controls" id="inputEmail4" placeholder="Enter Session Date"></input>
                     </div>
-                    <div className="form-group col-md-4 me-1 mx-5">
+                    <div className="form-group col-md-4 mx-5">
                         <input type="email" className="form-control controls" id="inputEmail4" placeholder="Enter Session Time"></input>
                     </div>
                 </div>
@@ -24,7 +24,12 @@ function NoteTaker() {
                 </div>
                 <div className="notetakerR1 m-5">
                     <div className="form-group col-md-12">
-                        <textarea type="email" className="form-control" id="textareaEmail4" placeholder="Start Taking Notes"></textarea>
+                        <textarea type="email" style={{ height: '30rem'}} className="form-control" id="New ID?" placeholder="Start Taking Notes"></textarea>
+                    </div>
+                </div>
+                <div className="notetakerR1">
+                    <div className="m-1">
+                        <button className="saveNoteBtn">Save</button>
                     </div>
                 </div>
             </form>
